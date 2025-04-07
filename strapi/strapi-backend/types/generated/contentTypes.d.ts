94b5874a1e48506617d067c5fe09a48f229e68f3
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-07 16:06:17 +0530

    completed

--- strapi/strapi-backend/types/generated/contentTypes.d.ts
+++ strapi/strapi-backend/types/generated/contentTypes.d.ts
@@ -384,6 +384,11 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    > &
+      Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
