9a73a16a9e6e39959ef57075dbdc5593dc343ce8
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-09 15:49:13 +0530

    completed localization in add and update

--- strapi/strapi-frontend/src/context/langContext.jsx
+++ strapi/strapi-frontend/src/context/langContext.jsx
@@ -1,24 +1,27 @@
+// langContext.js
 'use client'
-const { createContext, useState } = require("react");
+import { createContext, useState } from "react";
 
-// cpc
 export const langContextt = createContext();
+
 export const LangContextProvider = ({ children }) => {
-    const [selectLang, setSelectLang] = useState("English")
-    const getLocalized = (item, field) => {
-        if (selectLang === "Hindi") {
-          const hindiVersion =item.localizations?.find(loc => loc.locale === "hi-IN")
-          return hindiVersion?.[field] || item[field];
-        }
-        return item[field];
-      };
+  const [selectLang, setSelectLang] = useState("en"); // use 'en' or 'hi-IN' directly
+
+  const getLocalized = (item, field) => {
+    if (selectLang === "hi-IN") {
+      const hindiVersion = item.localizations?.find(loc => loc.locale === "hi-IN");
+      return hindiVersion?.[field] || item[field];
+    }
+    return item[field];
+  };
+
   return (
     <langContextt.Provider value={{
-        selectLang,
-        setSelectLang,
-        getLocalized
+      selectLang,
+      setSelectLang,
+      getLocalized
     }}>
       {children}
     </langContextt.Provider>
   );
-};  
+};
